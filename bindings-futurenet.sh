ca36ce4e680e7453da92ba0e99a8b20ef56ad4d1
Tyler van der Hoeven <hi@tyvdh.com>
2024-01-15 11:22:52 -0500

    slight update

--- bindings-futurenet.sh
+++ bindings-futurenet.sh
@@ -5,7 +5,7 @@
 
 contract_id=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/colorglyph.optimized.wasm --source default --network futurenet --fee 10000000)
 echo 'contract deployed' $contract_id
-# contract_id=CDSXSN5OWU4UHJLHZKWTZZSACTJSWTIQO3PZZLCKTNE5QOT3LFKIH2KG
+# contract_id=CC5YDEXJXBOBNUSHX52DIIAOOJJMACKFTFU7FRIWJRRNZG2K5YRCETVW
 
 token_address=CB64D3G7SM2RTH6JSGG34DDTFTQ5CFDKVDZJZSODMCX4NJ2HV2KN7OHT
 
@@ -16,5 +16,5 @@ fee_address=GA55USY2TY4DEO5YFQ3KZECL2A3A5IVYVCKPB4LLTAE57TOE6PM46D7C
 soroban contract invoke --id $contract_id --source default --network futurenet -- initialize --token_address $token_address --fee_address $fee_address
 echo 'contract initialized'
 
-soroban contract bindings typescript --wasm target/wasm32-unknown-unknown/release/colorglyph.optimized.wasm --contract-id $contract_id --output-dir ./colorglyph-sdk --overwrite
+soroban contract bindings typescript --wasm target/wasm32-unknown-unknown/release/colorglyph.optimized.wasm --contract-id $contract_id --network futurenet --output-dir ./colorglyph-sdk --overwrite
 echo 'bindings generated'
